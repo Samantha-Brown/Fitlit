3f75e1efd58d7c733d81e83965f613ed4ef2c241
Matt Roden <mattroden92@gmail.com>
2021-07-15 17:44:54 -0600

    Write more tests

--- test/Sleep-test.js
+++ test/Sleep-test.js
@@ -91,7 +91,139 @@ describe.only('Sleep', () => {
   }
 ];
     const sleep = new Sleep(sleepData);
-    expect(sleep.getAverageHoursSlept(1)).to.equal(6.1);
+    expect(sleep.getUserAverageHoursSlept(1)).to.equal(6.1);
+  })
 
+  it('should find a user\'s all time average sleep quality', () => {
+    const sleepData = [
+      {
+    "userID": 1,
+    "date": "2019/06/15",
+    "hoursSlept": 6.1,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "hoursSlept": 7,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "hoursSlept": 10.8,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "hoursSlept": 6.1,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/16",
+    "hoursSlept": 7,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/16",
+    "hoursSlept": 10.8,
+    "sleepQuality": 4.7
+  }
+];
+    const sleep = new Sleep(sleepData);
+    expect(sleep.getAverageSleepQuality(2)).to.equal(4.7);
+    expect(sleep.getAverageSleepQuality(3)).to.equal(4.7);
+    expect(sleep.getAverageSleepQuality(1)).to.equal(2.2);
+  })
+
+  it('should show the user the hours slept for a specific date', () => {
+    const sleepData = [
+      {
+    "userID": 1,
+    "date": "2019/06/15",
+    "hoursSlept": 6.1,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "hoursSlept": 7,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "hoursSlept": 10.8,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "hoursSlept": 6.1,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/16",
+    "hoursSlept": 7,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/16",
+    "hoursSlept": 10.8,
+    "sleepQuality": 4.7
+  }
+];
+    const sleep = new Sleep(sleepData);
+    expect(sleep.getHoursSleptByDate(1, "2019/06/15")).to.equal(6.1);
+    expect(sleep.getHoursSleptByDate(2, "2019/06/16")).to.equal(7);
+  });
+
+  it('should show the user their sleep quality for a specific date', () => {
+    const sleepData = [
+      {
+    "userID": 1,
+    "date": "2019/06/15",
+    "hoursSlept": 6.1,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/15",
+    "hoursSlept": 7,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/15",
+    "hoursSlept": 10.8,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 1,
+    "date": "2019/06/16",
+    "hoursSlept": 6.1,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 2,
+    "date": "2019/06/16",
+    "hoursSlept": 7,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 3,
+    "date": "2019/06/16",
+    "hoursSlept": 10.8,
+    "sleepQuality": 4.7
+  }
+];
+    const sleep = new Sleep(sleepData);
+    expect(sleep.getSleepQualityByDate(1, "2019/06/15")).to.equal(2.2);
+    expect(sleep.getSleepQualityByDate(2, "2019/06/16")).to.equal(4.7);
   })
 });
