8c8ea0f147ecb8bddf07cbb5e89463b545587ea5
Matt Roden <mattroden92@gmail.com>
2021-07-15 17:44:45 -0600

    Pass more tests

--- src/Sleep.js
+++ src/Sleep.js
@@ -1,14 +1,28 @@
 class Sleep {
   constructor(sleepData) {
-    this.sleepData = sleepData;
+    this.sleepData = sleepData; //[{}, {}, ...,{}]
   }
-  getAverageHoursSlept() {
+  getUserAverageHoursSlept(id) {
     let totalHours = 0
-    let newArr = this.sleepData.filter((sleep) => sleep.userID === 1);
+    let newArr = this.sleepData.filter((sleep) => sleep.userID === id);
     newArr.forEach((sleep) => (totalHours += sleep.hoursSlept));
-      console.log(totalHours);
     let avgHours = totalHours/newArr.length
     return avgHours;
+  }
+  getAverageSleepQuality(id) {
+    let totalQuality = 0
+    let newArr = this.sleepData.filter((sleep) => sleep.userID === id);
+    newArr.forEach((sleep) => (totalQuality += sleep.sleepQuality));
+    let avgQuality = totalQuality/newArr.length;
+    return avgQuality;
+  }
+  getHoursSleptByDate(id, date) {
+    let hoursForDay = this.sleepData.filter((sleep) => (sleep.userID === id && sleep.date === date))[0].hoursSlept;
+    return hoursForDay;
+  }
+  getSleepQualityByDate(id, date) {
+    let qualityForDay = this.sleepData.filter((sleep) => (sleep.userID === id && sleep.date === date))[0].sleepQuality;
+    return qualityForDay;
   }
 }
 
