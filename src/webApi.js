b7614c4ce96dc5054899f0881f058e15194cb914
Samantha Brown <bro7445@gmail.com>
2021-07-16 14:27:39 -0400

    Update webAPI.js to match main

--- src/webApi.js
+++ src/webApi.js
@@ -1,3 +1,21 @@
-let retrieveData = fetch("http:localhost:3001/api/v1/users")
+export function usersData() {
+  return fetch('http://localhost:3001/api/v1/users')
   .then(response => response.json())
-  .then(data => console.log(data));
+}
+
+export function retrieveSleepData() {
+  return fetch('http://localhost:3001/api/v1/sleep')
+  .then(response => response.json())
+}
+
+export function retrieveActivityData() {
+  return fetch('http://localhost:3001/api/v1/activity')
+  .then(response => response.json())
+}
+
+export function retrieveHydrationData() {
+  return fetch('http://localhost:3001/api/v1/hydration')
+  .then(response => response.json())
+};
+
+//retrieveData('sleep');
