fbf8bbc5e06bb0dc165d05f82f4c56c8e725fb94
Samantha Brown <bro7445@gmail.com>
2021-07-17 19:47:17 -0400

    Move friendsList function to scripts-commented out

--- src/scripts.js
+++ src/scripts.js
@@ -61,3 +61,16 @@ usersData() // Same as: Promise.then(data =>){}
   // .forEach((user) => {
   //   userRepoShit.push(user);
   // })
+
+  // <<<<Friends Array>>>>>
+  // findFriendsNames() {
+  //   console.log(this.friends.map((friendId) => (userRepoData.getUserData(friendId).name)));
+  //   return this.friends.map((friendId) => (userRepoData.getUserData(friendId).name));
+
+    //output will be array of strings
+    //input id numbers if this.friends array
+    //we need to access this.friends array
+    //then for each element in the array(number)
+    //pass that number as the argument of findAUser
+    //at this point we will have an array of friends objects
+    //access new friends array and return each friends getFirstName
