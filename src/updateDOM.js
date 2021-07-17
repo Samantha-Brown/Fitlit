0a8ba2dab6624478f7482b532b0d5cb0e7dfc6d6
Matt Roden <mattroden92@gmail.com>
2021-07-17 12:44:11 -0600

    Make changes to updateDOM, not in use yet

---
+++ src/updateDOM.js
@@ -0,0 +1,9 @@
+import User from './User';
+
+export function grabUserData() {
+  document.getElementById('h1').innerText = `${user.getFirstName()}`;
+  // more user stuff here
+  // more dom user stuff here
+  //MORE!!!!
+  // could export this function
+};
