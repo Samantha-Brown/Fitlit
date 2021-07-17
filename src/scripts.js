56b6f8dc829800611e20a6a4d4b6b59c10731e82
Matt Roden <mattroden92@gmail.com>
2021-07-17 16:05:11 -0600

    Display user first name and email

--- src/scripts.js
+++ src/scripts.js
@@ -1,36 +1,20 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS file
 import './css/styles.css';
 import {usersData, retrieveSleepData, retrieveActivityData, retrieveHydrationData} from './webApi.js';
-// import {grabUserData} from './updateDOM.js';
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-
-console.log('This is the JavaScript entry file - your code begins here.');
-
-// An example of how you tell webpack to use a JS file
-
-//import userData from './data/users';
 import UserRepository from './UserRepository';
 import User from './User';
 import Hydration from './Hydration';
 import Sleep from './Sleep';
 
+let sleepData, userRepoData, hydrationData, activityData, user;
 
 const getRandomIndex = () => {return Math.floor(Math.random() * 50)};
 const index = getRandomIndex();
 
 
-let sleepData, userRepoData, hydrationData, activityData, user;
-
 const grabAndImplementUserData = () => {
-  document.getElementById('h1').innerText = `${user.getFirstName()}`;
-  // more user stuff here
-  // more dom user stuff here
-  //MORE!!!!
-  // could export this function
+  document.getElementById('welcomeMessage').innerText = `Welcome, ${user.getFirstName(index)}`;
+  document.getElementById('email').innerText = `Email: ${user.email}`;
+  document.getElementById('email').innerText = `Email: ${user.email}`;
 };
 
 const forFun = () => {
@@ -52,17 +36,17 @@ usersData() // Same as: Promise.then(data =>){}
     // console.log(userRepoData.data[20])
     user = new User(userRepoData.data[index])
     // sleep = new Sleep(user)
-    // console.log(user);
+    console.log(user, '<>>>>USERRRR');
     grabAndImplementUserData();
   })
   // .then(grabAndImplementUserData)
 
-retrieveSleepData()
-  .then((data) => {
-    sleepData = new Sleep(data.sleepData);
-    console.log(sleepData);
-    grabAndImplementSleepData();
-  })
+// retrieveSleepData()
+//   .then((data) => {
+//     sleepData = new Sleep(data.sleepData);
+//     console.log(sleepData);
+//     grabAndImplementSleepData();
+//   })
   // .then(grabAndImplementSleepData);
 
 
