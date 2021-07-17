fbad09da2a2a5e1dcc93641d75a9b33f4c7c108f
Matt Roden <mattroden92@gmail.com>
2021-07-17 12:43:19 -0600

    Add successful sleep fetch call

--- src/scripts.js
+++ src/scripts.js
@@ -4,7 +4,7 @@
 // An example of how you tell webpack to use a CSS file
 import './css/styles.css';
 import {usersData, retrieveSleepData, retrieveActivityData, retrieveHydrationData} from './webApi.js';
-
+// import {grabUserData} from './updateDOM.js';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
@@ -15,26 +15,55 @@ console.log('This is the JavaScript entry file - your code begins here.');
 //import userData from './data/users';
 import UserRepository from './UserRepository';
 import User from './User';
+import Hydration from './Hydration';
+import Sleep from './Sleep';
+
+
+const getRandomIndex = () => {return Math.floor(Math.random() * 50)};
+const index = getRandomIndex();
+
 
 let sleepData, userRepoData, hydrationData, activityData, user;
 
-const grabDataFunction = () => {
+const grabAndImplementUserData = () => {
   document.getElementById('h1').innerText = `${user.getFirstName()}`;
   // more user stuff here
   // more dom user stuff here
   //MORE!!!!
   // could export this function
+};
+
+const forFun = () => {
+  console.log('hello', '<<<>>>>>FUN')
 }
+
+const grabAndImplementSleepData = () => {
+  let num = sleepData.findAUser(index)
+  console.log(sleepData, '<SLEEPDATA>>>', num, '<NUM>>>')
+  document.getElementById('anotha1').innerText = `${sleepData.getUserAverageHoursSlept(index)}`
+}
+
+
 //console.log(usersData())
 usersData() // Same as: Promise.then(data =>){}
   .then((data) => {
-    userRepoData = new UserRepository(data.userData);
-    // console.log(userRepoData.data[20])
-    user = new User(userRepoData.data[20])
-    // console.log(user);
-}).then(grabDataFunction);
+    userRepoData = new UserRepository(data.userData); //userRepoData is an object!
 
-retrieveHydrationData()
+    // console.log(userRepoData.data[20])
+    user = new User(userRepoData.data[index])
+    // sleep = new Sleep(user)
+    // console.log(user);
+    grabAndImplementUserData();
+  })
+  // .then(grabAndImplementUserData)
+
+retrieveSleepData()
+  .then((data) => {
+    sleepData = new Sleep(data.sleepData);
+    console.log(sleepData);
+    grabAndImplementSleepData();
+  })
+  // .then(grabAndImplementSleepData);
 
 
 
