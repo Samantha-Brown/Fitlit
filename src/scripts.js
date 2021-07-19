88254f7b294a102671c5ecbcd76b082e03a18899
Matt Roden <mattroden92@gmail.com>
2021-07-19 16:50:47 -0600

    Clean scripts

--- src/scripts.js
+++ src/scripts.js
@@ -35,57 +35,25 @@ const grabAndImplementHydrationData = () => {
   document.getElementById('weekOfWater').innerText = `This week's water: ${hydrationData.findWeeklyOunces(index, date)}`;
 }
 
-//console.log(usersData())
 retrieveUsersData() // Same as: Promise.then(data =>){}
-// import this function
   .then((data) => {
     userRepoData = new UserRepository(data.userData); //userRepoData is an object!
-
-    // console.log(userRepoData.data[20])
     user = new User(userRepoData.data[index])
     console.log(user, '<>>>>USERRRR');
     grabAndImplementUserData();
-  })
-
+  });
 
 retrieveSleepData()
   .then((data) => {
     sleepData = new Sleep(data.sleepData);
+    console.log(sleepData. '<<>>SLEEPDATA<<>>')
     console.log(sleepData);
-    //date = '2020/01/22'   // sleepData[9950].date;
-    console.log(date, '<<>>DATE<<>>')
-
     grabAndImplementSleepData();
   });
 
   retrieveHydrationData()
   .then((data) => {
     hydrationData = new Hydration(data.hydrationData);
-    //date = '2020/01/22'
     console.log(hydrationData, "<<<<<HERE>>>>>>");
     grabAndImplementHydrationData();
-  })
-
-
-
-
-
-  // console.log(data.userData);
-  //need to call class methods with this data
-  // user and userRepo data are both scoped inside of this function
-  // .forEach((user) => {
-  //   userRepoShit.push(user);
-  // })
-
-  // <<<<Friends Array>>>>>
-  // findFriendsNames() {
-  //   console.log(this.friends.map((friendId) => (userRepoData.getUserData(friendId).name)));
-  //   return this.friends.map((friendId) => (userRepoData.getUserData(friendId).name));
-
-    //output will be array of strings
-    //input id numbers if this.friends array
-    //we need to access this.friends array
-    //then for each element in the array(number)
-    //pass that number as the argument of findAUser
-    //at this point we will have an array of friends objects
-    //access new friends array and return each friends getFirstName
+  });
