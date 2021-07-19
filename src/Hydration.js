afa82c163a38e815a41558d706b014f2b91d1c88
Matt Roden <mattroden92@gmail.com>
2021-07-18 18:34:28 -0600

    Get selectWeek function to work

--- src/Hydration.js
+++ src/Hydration.js
@@ -3,49 +3,32 @@ class Hydration {
     this.hydrationData = hydrationData;
   }
 
-findAUser(id) {
-  return this.hydrationData.filter(aUser => aUser.userID === id)
-
+  findAUser(id) {
+    return this.hydrationData.filter(aUser => aUser.userID === id)
+  }
+  calcAverageOunces(id) {
+    let totalOunces = 0
+    let perDayHydration = this.hydrationData.filter((data) => id === data.userID)
+    perDayHydration.forEach((hydration) => (totalOunces += hydration.numOunces));
+    let avgOunces = Math.round(totalOunces/perDayHydration.length)
+    return avgOunces;
+  }
+  findByDate(dateSelected) {
+  return this.hydrationData.find(data => data.date === dateSelected).numOunces;
+  }
+  selectWeek(id, date) {
+    let foundUser = this.findAUser(id); //array of all specific user hydration data
+    let foundUserObj = foundUser.find((user) => user.date === date); // singular object containing inputed date arg
+    let selection = foundUser.indexOf(foundUserObj) + 1;
+    let foundWeek = foundUser.slice(selection -7, selection);
+    return foundWeek;
+    // return foundWeek.map((day) => day.numOunces);
+  }
+  findWeeklyOunces(id, date) {
+    return this.selectWeek(id, date).map((day) => day.numOunces);
+  };
 }
 
-calcAverageOunces(id) {
-  let totalOunces = 0
-  let perDayHydration = this.hydrationData.filter((data) => id === data.userID)
-  perDayHydration.forEach((hydration) => (totalOunces += hydration.numOunces));
-  let avgOunces = totalOunces/perDayHydration.length
-  return avgOunces;
-}
-
-findByDate(dateSelected) {
-return this.hydrationData.find(data => data.date === dateSelected).numOunces;
-}
-
-selectWeek(id) {
-  let user = this.findAUser(id)
-  return user.slice(-7);
-}
-
-findWeeklyOunces() {
-  this.selectWeek()
-  let displaySevenDays = []
-  this.hydrationData.forEach((day) => {
-  displaySevenDays.push(day.numOunces);
-  });
-
-  // this.hydrationData.filter((day) => day.numOunces)
-  // .forEach((day) => displaySevenDays.push(day.numOunces));
-  console.log('>>>>>>', displaySevenDays);
-  return displaySevenDays;
-}
-}
-  // Create classes and methods that can calculate:
-
-// For a user (identified by their userID - this is the same for all methods
-// requiring a specific user’s data), the average fluid ounces consumed per day
-// for all time
-
-// For a user, how many fluid ounces of water consumed each day over the course
-// of a week (7 days) - return the amount for each day
 
 
 export default Hydration;
