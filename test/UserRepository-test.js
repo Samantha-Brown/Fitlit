40776d7cb9de1758fbc21181de5556fcae75ceff
Matt Roden <mattroden92@gmail.com>
2021-07-19 18:29:48 -0600

    Write test for getting a user friends list

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -3,7 +3,6 @@ import UserRepository from '../src/UserRepository';
 
 describe('User Repository', () => {
   let userRepo = [{
-    // Take off JSON strings
     "id": 1,
     "name": "Luisa Hane",
     "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
@@ -11,7 +10,6 @@ describe('User Repository', () => {
     "strideLength": 4.3,
     "dailyStepGoal": 10000,
     "friends": [
-      16,
       4,
       8
     ]
@@ -43,7 +41,36 @@ describe('User Repository', () => {
       42,
       33
     ]
-  },];
+  },
+  {
+    "id": 4,
+    "name": "Mae Connelly",
+    "address": "28926 Schinner Islands, Turnermouth NE 23720-3230",
+    "email": "Marcos_Pollich@hotmail.com",
+    "strideLength": 3.1,
+    "dailyStepGoal": 4000,
+    "friends": [
+      48,
+      7,
+      44,
+      8
+    ]
+  },
+  {
+    "id": 8,
+    "name": "Laney Abshire",
+    "address": "86416 Koch Inlet, North Kaciefurt MA 80635",
+    "email": "Janice_Nitzsche2@yahoo.com",
+    "strideLength": 2.8,
+    "dailyStepGoal": 2000,
+    "friends": [
+      11,
+      41,
+      23,
+      49
+    ]
+  },
+];
 
   beforeEach(() => {
     let repo = new UserRepository(userRepo);
@@ -73,5 +100,10 @@ describe('User Repository', () => {
   it('should return the average step goal for all users', () => {
     let repo = new UserRepository(userRepo);
     expect(repo.getAverageStepGoal()).to.equal(6667)
+  })
+
+  it.only('should return a user\'s friends by name', () => {
+    let repo = new UserRepository(userRepo);
+    expect(repo.getUserFriendsNames(1)).to.deep.equal(["Mae Connelly", "Laney Abshire"]);
   })
 });
