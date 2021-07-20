9168c9e95fbc8f9f84147d65e577bfc37fdb8d15
Samantha Brown <bro7445@gmail.com>
2021-07-20 19:04:50 -0400

    comment out the updateDom file to please the linter

--- src/updateDOM.js
+++ src/updateDOM.js
@@ -1,7 +1,7 @@
-import User from './User';
-import { retrieveUsersData } from './scripts.js'
-
-const getRandomIndex = () => {
-  return Math.floor(Math.random() * 50)
-};
-const index = getRandomIndex();
+// import User from './User';
+// import { retrieveUsersData } from './scripts.js'
+//
+// const getRandomIndex = () => {
+//   return Math.floor(Math.random() * 50)
+// };
+// const index = getRandomIndex();
