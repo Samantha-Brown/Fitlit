2c619825eefbfc95ecc6ef88770e2dbd2c719927
Matt Roden <mattroden92@gmail.com>
2021-07-19 18:46:02 -0600

    Fix indentation

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -6,13 +6,11 @@ class UserRepository {
   getUserData(id) {
     return this.data.find((user) => user.id === id);
   }
-
   getAverageStepGoal() {
     let goals = 0;
     this.data.forEach((user) => goals += user.dailyStepGoal)
     return Math.round(goals/this.data.length);
   }
-
   findAUser(id) {
     return this.userData.filter(aUser => aUser.userID === id);
   }
@@ -27,10 +25,3 @@ class UserRepository {
 }
 
 export default UserRepository;
-
-
-// filter through the user data and grab the dailyStepGoals
-// those get put in their own arrray then
-// add each element in the array together &
-// divide by this.data.lenght
-// ForEach +=
